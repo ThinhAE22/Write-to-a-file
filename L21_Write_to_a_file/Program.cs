6e250b3ec306fac52425d2c1152b700e4da54819
Thinh Tran <153496070+ThinhAE22@users.noreply.github.com>
2024-01-24 15:05:57 +0200

    Add files via upload

---
+++ L21_Write_to_a_file/Program.cs
@@ -0,0 +1,19 @@
+﻿// read from a file
+using System;
+using System.IO;
+
+class FileWriter
+{
+    static void Main()
+    {
+        string fileName = "test.txt";
+        string content = "Some content more";
+
+        using (StreamWriter sw = new StreamWriter(fileName, true))
+        // the true boolean value use to append the existed file
+        {
+            sw.WriteLine(content);
+            sw.Close();
+        }
+    }
+}
